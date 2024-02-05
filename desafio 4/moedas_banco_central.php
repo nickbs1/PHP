7d05d7c1fa9bb56bbc644f9c69255430c2f8f0a4
nick <nickenzo@outlook.com.br>
2024-02-05 12:38:20 -0300

    desafio 4 incompleto

--- desafio 4/moedas_banco_central.php
+++ desafio 4/moedas_banco_central.php
@@ -1,13 +1,24 @@
 <pre>
 <?php 
-$url='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'01-29-2024\'&@dataFinalCotacao=\'02-02-2024\'&$top=1&$orderby=DataHoraCota%C3%A7%C3%A3o%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+$inicio = date("m-d-Y",strtotime("-7 days"));
+$fim = date("m-d-Y");
+$url='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $fim . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
 
 
 $dados=json_decode(file_get_contents($url),true);
 
-var_dump($dados);
+//var_dump($dados);
 
+$cotação=$dados["value"][0]["cotacaoCompra"];
+$dia=$dados["value"][0] ["dataHoraCotacao"];
+
+
+
+echo"cotação do dolar hoje $cotação <br> dia $dia";
 
 ?>
+<!--
+video desafio 4 de php curso em video do gustavo guanabara.
+-->
 </pre>
--- desafio 4/moedasbd.php
+++ desafio 4/moedasbd.php
@@ -9,9 +9,26 @@
 <body>
 <main>  
 <?php 
-$cotaçao=4.95;
-$real=$_GET["din"];
-$dolar=$real/$cotaçao;
+
+$inicio = date("m-d-Y",strtotime("-7 days"));
+$fim = date("m-d-Y");
+$url='https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $fim . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+
+
+$dados=json_decode(file_get_contents($url),true);
+
+$cotação=$dados["value"][0]["cotacaoCompra"];
+$dia=$dados["value"][0] ["dataHoraCotacao"];
+
+echo"cotação do dolar hoje $cotação <br> dia $dia";
+
+
+
+
+
+$real=$_REQUEST["din"];
+$dolar=$real / $cotação;
 $padrão=numfmt_create("pt_BR",NumberFormatter::CURRENCY);
 echo"seus " . numfmt_format_currency($padrão,$real,"BRL") 
 . "<br>equivale " . numfmt_format_currency($padrão,$dolar,"USD");

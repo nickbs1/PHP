04c31fffc6e7887f50f12493b5b6ac1265882d2a
nick <nickenzo@outlook.com.br>
2024-02-05 10:20:31 -0300

    Desafio 2 explicado

--- desafio 2/numeros_aleatorios.php
+++ desafio 2/numeros_aleatorios.php
@@ -10,12 +10,33 @@
   <main>
 <?php 
 $min=0;
+/* 
+$min=0: declarei uma variavel chamada $min com o valor 0.
+*/
 $max=105;
+/*
+$max= declarei uma variavel chamada $max com o valor 105
+*/
 $num=mt_rand($min,$max);
+/*
+1-mt_rand(): mt_rand é uma função em PHP que gera um número inteiro aleatório. 
+2-$min e $max: São as variáveis que representam os limites inferior e superior do intervalo no qual você deseja gerar o número aleatório.Neste caso vai gerar valores entre 0 até 105.
+3-$num: Esta variável armazenará o número inteiro aleatório gerado pela função mt_rand dentro do intervalo definido pelos valores de $min e $max.
+*/
 echo"<h1>Gerador de numero entre 0 e 105.</h1>";
-echo"<br> $num"
+echo"<br> $num";
+/*
+1-vai imprimir na tela Gerador de numero entre 0 e 105.
+2-<h1> e </h1>=é uma tag em html que coloca a frase em cabeçalho de nível 1.
+<br>= quebra de linha em html.
+*/
+
 ?>
 <button onclick="javascript:document.location.reload()">Gerar</button>
+<!--
+essa linha de codigo foi explicada no desafio 1.
+em resumo ela recarrega a pagina.
+-->
   </main>  
 </body>
 </html>

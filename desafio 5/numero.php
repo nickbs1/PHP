c12d82353b5ae76c0d6af7d8b62e84d04d0213aa
nick <nickenzo@outlook.com.br>
2024-02-05 16:59:56 -0300

    começando o exercicio 5

--- desafio 5/numero.php
+++ desafio 5/numero.php
@@ -9,11 +9,14 @@
 <body>
   <main>
 <?php 
-
+$num=$_POST["numero"];
+$inteira=(int) $num;
+$fracionada=$num-$inteira;
+echo"O numero digitado foi $num <br> A parte inteira é:$inteira <br> A parte fracionada é: $fracionada";
 
 
 ?>
-
+<button onclick="javascript:history.go(-1)">Voltar</button>
 
 
   </main>  
